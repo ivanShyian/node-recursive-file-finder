ccbe63fe08a9e4984bd410235eaf6eef38dcc01f
vanjkes <shyian.ivan@yahoo.com>
2021-06-04 18:16:44 +0300

    Added import of readline :))

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const path = require('path')
 const fs = require('fs')
+const readline = require('readline')
 
-function recFindByExt(base,ext,files,result) {
+const recFindByExt = (base, ext, files, result) => {
   files = files || fs.readdirSync(base)
-  result = result || []
+  result = result || {}
 
   files.forEach(file => {
       const newbase = path.join(base, file)
       if (fs.statSync(newbase).isDirectory()) {
-        result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
+        result = recFindByExt(newbase, ext, fs.readdirSync(newbase), result)
       }
       else {
-        if (file.substr(-1*(ext.length+1)) === '.' + ext) {
+        if (file.substr(-1 * (ext.length + 1)) === '.' + ext) {
           const exactFile = newbase.split('/')
-          result.push(exactFile[exactFile.length - 1])
+          result[exactFile[exactFile.length - 1]] = '1'
+          console.log(result)
         }
       }
     }
@@ -24,7 +26,7 @@ function recFindByExt(base,ext,files,result) {
 const helper = (ext) => {
   const final = []
   for(let i = 0; i < ext.length; i++) {
-    final.push(...recFindByExt(path.join(__dirname, '../', 'inrating.top', 'src'),ext[i]))
+    final.push(recFindByExt(path.join(__dirname, '../', 'inrating.top', 'src'),ext[i]))
   }
   return final
 }
