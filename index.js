b1734a07ac4cd15ee0c77beb05eef28de678d3d1
vanjkes <shyian.ivan@yahoo.com>
2021-06-06 01:03:35 +0300

    Stuck :(

--- index.js
+++ index.js
@@ -2,46 +2,42 @@ const path = require('path')
 const fs = require('fs')
 const lineReader = require('line-reader')
 
-
 const recFindByExt = (base, ext, keywords, separator, files, result) => {
   files = files || fs.readdirSync(base)
   result = result || {}
 
   files.forEach(file => {
-      const newbase = path.join(base, file)
-      if (fs.statSync(newbase).isDirectory()) {
-        result = recFindByExt(newbase, ext, keywords, separator, fs.readdirSync(newbase), result)
-      } else {
-        if (file.substr(-1 * (ext.length + 1)) === '.' + ext) {
-          const filePathArray = newbase.split(separator)
-          const fileName = filePathArray[filePathArray.length - 1]
+    const newbase = path.join(base, file)
+    if (fs.statSync(newbase).isDirectory()) {
+      result = recFindByExt(newbase, ext, keywords, separator, fs.readdirSync(newbase), result)
+    } else {
+      if (file.substr(-1 * (ext.length + 1)) === '.' + ext) {
+        const filePathArray = newbase.split(separator)
+        const fileName = filePathArray[filePathArray.length - 1]
 
-          lineReader.eachLine(newbase, (line, last) => {
-            if (line.search(keywords[0]) !== -1 || line.search(keywords[1]) !== -1) {
-
-              if (result.hasOwnProperty(fileName)) {
-                for (let key in result[fileName]) {
-                  if (result[fileName].hasOwnProperty(key)) {
-                    if (result[fileName][key].trim() === line.trim()) {
-                      return
-                    }
-                    result[fileName] = {
-                      ...result[fileName],
-                      [Object.keys(result[fileName]).length]: line.trim()
-                    }
-                    return
+        lineReader.eachLine(newbase, (line, last) => {
+          if (last) {
+            return false
+          }
+          if (line.search(keywords[0]) !== -1 || line.search(keywords[1]) !== -1) {
+            if (result.hasOwnProperty(fileName)) {
+              for (let key in result[fileName]) {
+                if (result[fileName].hasOwnProperty(key) && result[fileName][key].trim() !== line.trim()) {
+                  return result[fileName] = {
+                    ...result[fileName],
+                    [Object.keys(result[fileName]).length]: line.trim()
                   }
                 }
               }
-              result[fileName] = {
-                0: line.trim()
-              }
             }
-          })
-        }
+            return result[fileName] = {
+              0: line.trim()
+            }
+          }
+        })
       }
     }
-  )
+  })
   return result
 }
 
@@ -50,8 +46,8 @@ const helper = (ext, keywords, separator) => {
   for (let i = 0; i < ext.length; i++) {
     final.push(recFindByExt(
       path.join(__dirname, '../../$vue-js/$vue-inrating/inrating.top', 'src'),
-      ext[i],
-      keywords,
+      searchFiles[i],
+      searchArray,
       separator
     ))
   }
@@ -63,4 +59,5 @@ const searchArray = [/api\./gmi, /\$http\./gmi]
 const separator = '\\'
 
 const res = helper(searchFiles, searchArray, separator)
+
 console.log(res)
