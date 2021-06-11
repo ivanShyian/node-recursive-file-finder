df5b19a1291a22664ef5703825262ebc59dddbd9
vanjkes <shyian.ivan@yahoo.com>
2021-06-11 18:38:53 +0300

    Added function that opens api file and working with it

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const path = require('path')
 const fs = require('fs')
 const readline = require('readline')
 
+
+const separator = '/'
+const mainDir = '../inrating.top/src'
+const fileExtensions = ['vue', 'js', 'ts', 'vue.ts']
+const listOfKeywords = [/api\./gm, /\$http\./gm]
+const apiFilePath = '../inrating.top/src/common-components/api/api.ts'
+const listOfKeywordsForApi = [
+  /(api\.)(\w+)(\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?/m,
+  /(\$http\.)(\w+)(\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?/m
+]
+
 const recFindByExt = async(base, ext, keywords, separator, files, result) => {
   files = files || fs.readdirSync(base)
   result = result || {}
@@ -30,6 +41,8 @@ const lineReaderHelper = async(pathname, fileName, keywords, resultObj) => {
 
   for await (const line of r1) {
     if (line.search(keywords[0]) !== -1 || line.search(keywords[1]) !== -1) {
+      await findExactApiPath(line, keywords)
+
       if (result.hasOwnProperty(fileName)) {
         const res = Object.keys(result[fileName])
           .map(i => result[fileName][i].trim())
@@ -52,6 +65,38 @@ const lineReaderHelper = async(pathname, fileName, keywords, resultObj) => {
   return result
 }
 
+// <<Temp.. Do not try this at home (Temporary..)
+const findExactApiPath = async(line, keywords) => {
+  const res = line.match(listOfKeywordsForApi[0]) || line.match(listOfKeywordsForApi[1])
+  const apiRouteArray = res.filter((i, index) => index !== 0 
+    && index !== 1 
+    && i !== '.' 
+    && typeof i === 'string'
+  )
+
+  const apiName = apiRouteArray[apiRouteArray.length - 1]
+  const regEx1 = new RegExp(apiName, 'm')
+
+  const secondFileStream = fs.createReadStream(apiFilePath)
+  const r1 = readline.createInterface({
+    input: secondFileStream,
+    crlfDelay: Infinity
+  })
+
+  let nextLine = false
+
+
+  for await (const lineFromStream of r1) {
+    if (nextLine) {
+      nextLine = false
+    }
+
+    if (lineFromStream.search(regEx1) !== -1) {
+      nextLine = true
+    }
+  }
+}
+// Temp>>
 
 const runner = async(pathSource, files, regex, separator) => {
   const finalArray = []
@@ -69,7 +114,11 @@ const runner = async(pathSource, files, regex, separator) => {
 
 
 (async () => {
-  const res = await runner('../inrating.top/src', ['vue', 'js', 'ts', 'vue.ts'], [/api\./gmi, /\$http\./gmi], '/')
-  console.log(res)
+  const res = await runner(
+      mainDir, 
+      fileExtensions, 
+      listOfKeywords, 
+      separator
+    )
 })()
 
