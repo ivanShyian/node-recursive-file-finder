660b4c2da63a3add12944b99e42a8e55e415e1e9
vanjkes <shyian.ivan@yahoo.com>
2021-06-14 13:13:19 +0300

    Commited actual version of plugin

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fs = require('fs')
 const readline = require('readline')
 
 
-const separator = '\\'
-const mainDir = '../../$vue-js/$vue-inrating/inrating.top/src'
+const separator = '/'
+const mainDir = '../inratingwebmobile/src/common-components'
 const fileExtensions = ['vue', 'js', 'ts', 'vue.ts']
 const listOfKeywords = [/api\./gm, /\$http\./gm]
 const listOfFilesToExclude = ['api.ts']
-const apiFilePath = '../../$vue-js/$vue-inrating/inrating.top/src/common-components/api/api.ts'
+const apiFilePath = '../inratingwebmobile/src/common-components/api/api.ts'
 const listOfKeywordsForApi = [
-  /(api\.)(\w+)(\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?/m,
-  /(\$http\.)(\w+)(\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?/m
+  /(api\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?(\.)?(\w+)?/m,
+  /(\$http\.)(\w+)(\.)?(\w+)?(\.)?(\w+)?(\.)?(\w+)?/m
 ]
 
-const recFindByExt = async(base, ext, keywords, toExclude, separator, files, result) => {
+const recFindByExt = async (base, ext, keywords, toExclude, separator, files, result) => {
   files = files || fs.readdirSync(base)
   result = result || {}
 
@@ -31,7 +31,7 @@ const recFindByExt = async(base, ext, keywords, toExclude, separator, files, res
   return result
 }
 
-const lineReaderHelper = async(pathname, fileName, keywords, resultObj) => {
+const lineReaderHelper = async (pathname, fileName, keywords, resultObj) => {
   const result = resultObj
   const fileStream = fs.createReadStream(pathname)
 
@@ -65,53 +65,54 @@ const lineReaderHelper = async(pathname, fileName, keywords, resultObj) => {
   return result
 }
 
-const findExactApiPath = async(objectOfItems, keywords, apiPath) => {
+const findExactApiPath = async (objectOfItems, keywords, apiPath) => {
   await Promise.all(Object.keys(objectOfItems).map(async file => {
     await Promise.all(Object.keys(objectOfItems[file]).map(async idx => {
-       const regex = objectOfItems[file][idx].match(listOfKeywordsForApi[0]) || objectOfItems[file][idx].match(listOfKeywordsForApi[1])
-       const apiRouteArray = regex.filter((i, index) =>
-         index !== 0
+      const regex = objectOfItems[file][idx].match(listOfKeywordsForApi[0]) || objectOfItems[file][idx].match(listOfKeywordsForApi[1])
+      const apiRouteArray = regex.filter((i, index) =>
+        index !== 0
           && index !== 1
-           && i !== '.'
-            && typeof i === 'string'
-       )
+            && i !== '.'
+              && typeof i === 'string'
+      )
 
-       const apiName = apiRouteArray[apiRouteArray.length - 1]
-       const regexOfApiName = new RegExp(` ${apiName}`, 'm')
-       const regexToExactApiPath = new RegExp('\'(.*)\'', 'm')
+      const apiName = apiRouteArray[apiRouteArray.length - 1]
+      const regexOfApiName = new RegExp(` ${apiName}`, 'm')
+      const regexToExactApiPath = new RegExp('\'(.*)\'', 'm')
 
-       const apiFileStream = fs.createReadStream(apiPath)
-       const rline = readline.createInterface({
-         input: apiFileStream,
-         crlfDelay: Infinity
-       })
+      const apiFileStream = fs.createReadStream(apiPath)
+      const rline = readline.createInterface({
+        input: apiFileStream,
+        crlfDelay: Infinity
+      })
 
-       let nextLine = false
-       for await (const lineFromStream of rline) {
+      let nextLine = false
+      for await (const lineFromStream of rline) {
 
-         if (nextLine) {
+        if (nextLine) {
 
-           nextLine = false
-           const finalLine = lineFromStream.match(regexToExactApiPath)
+          nextLine = false
+          const finalLine = lineFromStream.match(regexToExactApiPath)
 
-           if (finalLine === null) {
-             nextLine = true
-           } else {
-             objectOfItems[file] = {...objectOfItems[file], [idx]: finalLine[1]}
-           }
-         }
+          if (finalLine === null) {
+            nextLine = true
+          } else {
+            objectOfItems[file] = {...objectOfItems[file], [idx]: finalLine[1]}
+          }
+        }
 
-         if (lineFromStream.search(regexOfApiName) !== -1) {
-           nextLine = true
-         }
-       }
+        if (lineFromStream.search(regexOfApiName) !== -1) {
+          nextLine = true
+        }
+      }
     }))
   }))
   return objectOfItems
 }
 
-const runner = async(pathSource, files, regex, toExclude, separator, extraRegex, api) => {
+const runner = async (pathSource, files, regex, toExclude, separator, extraRegex, api) => {
   let finalObj = {}
+  const sortedFinalObj = {}
 
   await Promise.all(files.map(async file => {
     const result = await recFindByExt(
@@ -125,20 +126,25 @@ const runner = async(pathSource, files, regex, toExclude, separator, extraRegex,
     const res = await findExactApiPath(result, extraRegex, api)
     finalObj = {...finalObj, ...res}
   }))
+
+  Object.keys(finalObj).sort().forEach(e => {
+    sortedFinalObj[e] = finalObj[e]
+  })
+
   return finalObj
 }
 
 
 (async () => {
   const res = await runner(
-      mainDir,
-      fileExtensions,
-      listOfKeywords,
-      listOfFilesToExclude,
-      separator,
-      listOfKeywordsForApi,
-      apiFilePath
-    )
-  console.log(res)
+    mainDir,
+    fileExtensions,
+    listOfKeywords,
+    listOfFilesToExclude,
+    separator,
+    listOfKeywordsForApi,
+    apiFilePath
+  )
+  console.log(JSON.stringify(res))
 })()
 
